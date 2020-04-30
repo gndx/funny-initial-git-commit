4735ad8c594d5caff7f0fb1bcd53205b0bae2b46
vivi_soto <anavivi-24@hotmail.com>
2020-04-29 19:37:34 -0500

    [ADD] Frase de vivi_soto

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const messages = [
   "Reinventing the wheel. Again.",
   "This is not the commit message you are looking for",
   "Batman! (this commit has no parents)",
+  "Believe and act as if it were impossible to fail",
 ];
 
 const funnyCommit = () => {
