9faeb1c4870709da21ba227ac70ab63e6e4c32b7
vivi_soto <61467411+ViviSoto@users.noreply.github.com>
2020-04-30 20:08:35 -0500

    frase when

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const messages = [
   "Reinventing the wheel. Again.",
   "This is not the commit message you are looking for",
   "Batman! (this commit has no parents)",
+  "when a comma was the mistake you looked for an hour",
 ];
 
 const funnyCommit = () => {
