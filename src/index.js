0714073ee73c139ab76d36621881f6c9739034db
marcela62179 <43826366+marcela62179@users.noreply.github.com>
2020-05-02 19:35:50 -0500

    funny

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const messages = [
   "Reinventing the wheel. Again.",
   "This is not the commit message you are looking for",
   "Batman! (this commit has no parents)",
+  "hey girl, is your name google?  'cause you've got everything i´m searching for"
 ];
 
 const funnyCommit = () => {
