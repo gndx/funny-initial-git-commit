134115bb47a3080f52fef7bf3b2ef153e9d9333f
Juan Luis Chavez <juan_luis_xw@hotmail.com>
2020-05-05 15:20:12 -0500

    [ADD] add the phrase about programming

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const messages = [
   "Reinventing the wheel. Again.",
   "This is not the commit message you are looking for",
   "Batman! (this commit has no parents)",
+  "JuanLChavezM -- Programming is doing magic!",
 ];
 
 const funnyCommit = () => {
