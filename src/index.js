be002cf69cafa8cf307c1cd4ce58b1bb93afa457
Wilmar-Yoan-Malaver <59549112+Wilmar-Yoan-Malaver@users.noreply.github.com>
2020-05-12 15:38:11 -0500

    "Add new commit by wilmar malaver"

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const messages = [
   "Reinventing the wheel. Again.",
   "This is not the commit message you are looking for",
   "Batman! (this commit has no parents)",
+  "A picture is worth a thousand words . But it takes up more memory" ,
 ];
 
 const funnyCommit = () => {
