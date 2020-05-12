57f551a80bf0c71205a17afb9418be3b514dfbb7
Andres Loaiza <chelinandres@outlook.com>
2020-05-12 16:22:05 -0500

    chiste del dia, andres loaiza

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const messages = [
   "Reinventing the wheel. Again.",
   "This is not the commit message you are looking for",
   "Batman! (this commit has no parents)",
+  "https://github.com/andres1234loa/funny-initial-git-commit.git",
 ];
 
 const funnyCommit = () => {
