d22c7dad390dfd35592e5dff617706659619c1f2
Mayerly Prieto <mayojiza@gmail.com>
2020-05-12 16:41:13 -0500

    Frase Mayerly Prieto

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const messages = [
   "Reinventing the wheel. Again.",
   "This is not the commit message you are looking for",
   "Batman! (this commit has no parents)",
+  "A new adventure is waiting for you",
 ];
 
 const funnyCommit = () => {
