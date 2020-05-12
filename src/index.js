2be6c9104c1ebae54f6b97a54a21d1c42cbec723
Jose Burgon <burgonjose@gmail.com>
2020-05-12 14:33:00 -0500

    Winter is coming...

--- src/index.js
+++ src/index.js
@@ -11,13 +11,14 @@ const messages = [
   "Reinventing the wheel. Again.",
   "This is not the commit message you are looking for",
   "Batman! (this commit has no parents)",
+  "Winter is coming...",
 ];
 
 const funnyCommit = () => {
   const message = messages[Math.floor(Math.random() * messages.length)];
   console.log(`\x1b[34m${message}\x1b[89m`);
-}
+};
 
 module.exports = {
-  funnyCommit
+  funnyCommit,
 };
