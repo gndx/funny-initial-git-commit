97fbcd18b6a852308f5c99f784769f106be34865
Felipe Morales <tadeus87@gmail.com>
2020-05-12 11:46:51 -0500

    Add phrase as part of homework - empoderatech.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const messages = [
   "Reinventing the wheel. Again.",
   "This is not the commit message you are looking for",
   "Batman! (this commit has no parents)",
-  "There are only 10 types of people: those who understand binary and those who do not",
+  "There are only 10 types of people: those who understand binary and those who do not.",
 ];
 
 const funnyCommit = () => {
