a33182693a3c717d03d5bc8cc8089ff4c0850157
Richard Dev <rigiba@gmail.com>
2020-05-12 20:12:37 -0500

    [ADD] - Cambios en Funny index.js -> EmpoderaTech - R1ch4rd G1L

--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ const funnyCommit = () => {
   const message = messages[Math.floor(Math.random() * messages.length)];
   console.log(`\x1b[34m${message}\x1b[89m`);
 }
-
 module.exports = {
   funnyCommit
 };
