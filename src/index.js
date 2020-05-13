d0cd10dccd2601fc376ac3294ba033592f5fc878
Alexander Guaman Cruz <alexander.guaman.cruz@gmail.com>
2020-05-12 19:56:39 -0500

    Excellent application, try it is incredible

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ const messages = [
   "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
   "naruto, The programmer ninja!",
   "The covid-19 block my commit",
+  "Excellent application, try it is incredible"
 ];
 
 const funnyCommit = () => {
