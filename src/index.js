e7906df95f870a090a4d644d9e45d2037c5c0c54
Richard Gil <rigiba@gmail.com>
2020-05-13 10:51:16 -0500

    Update index.js
    [ADD]- Virus Funny

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ const messages = [
   "naruto, The programmer ninja!",
   "The covid-19 block my commit",
   "I love your blog, you'll say that to all your links",
+  "virus scan? aint nobody got time for that!",
 ];
 
 const funnyCommit = () => {
